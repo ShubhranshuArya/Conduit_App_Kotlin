7e8a0219fec6db3a9069b13347a72f8bcae1a110
ShubhranshuArya <developershubh69@gmail.com>
2021-08-28 11:36:31 +0530

    Added the basic api

--- api/src/main/java/com/example/api/services/ConduitApi.kt
+++ api/src/main/java/com/example/api/services/ConduitApi.kt
@@ -1,12 +1,27 @@
 package com.example.api.services
 
-import com.example.api.models.ArticlesResponse
-import retrofit2.Call
+import com.example.api.models.entities.UserCreds
+import com.example.api.models.requests.SignUpRequest
+import com.example.api.models.responses.ArticlesResponse
+import com.example.api.models.responses.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ConduitApi {
 
+    @POST("users")
+    suspend fun signUpUser(
+        @Body userCreds: SignUpRequest
+    ): Response<UserResponse>
+
     @GET("articles")
-    fun getArticles(): Call<ArticlesResponse>
+    suspend fun getArticles(
+        @Query("author") author: String? = null,
+        @Query("favourited") favourited: String? = null,
+        @Query("tags") tag: String? = null,
+    ): Response<ArticlesResponse>
 
 }
--- api/src/test/java/com/example/api/ConduitClientTests.kt
+++ api/src/test/java/com/example/api/ConduitClientTests.kt
@@ -1,15 +1,51 @@
 package com.example.api
 
+import com.example.api.models.entities.UserCreds
+import com.example.api.models.requests.SignUpRequest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
+import kotlin.random.Random
 
 class ConduitClientTests {
 
     private val conduitClient = ConduitClient()
 
     @Test
-    fun `Get Articles`() {
-        val articles = conduitClient.api.getArticles().execute()
-        assertNotNull(articles.body()?.articles)
+    fun `Get articles`() {
+        runBlocking {
+            val articles = conduitClient.api.getArticles()
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `Get articles by author`() {
+        runBlocking {
+            val articles = conduitClient.api.getArticles(author = "444")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `Get articles by tags`() {
+        runBlocking {
+            val articles = conduitClient.api.getArticles(tag = "dragons")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `Post users - create user`() {
+        val userCreds = UserCreds(
+            email = "testemail${Random.nextInt(999, 9999)}@test.com",
+            password = "pass${Random.nextInt(9999, 99999)}",
+            username = "rand_user_${Random.nextInt(99, 999)}"
+        )
+        runBlocking {
+            val resp = conduitClient.api.signUpUser(SignUpRequest(userCreds))
+            assertEquals(userCreds.username, resp.body()?.user?.username)
+        }
     }
 }
